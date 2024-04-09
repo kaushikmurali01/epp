0ef0b1e6dd79ff28e05cad0d3cb870f5c1fc544f
mohitRaw <mohit.rawat@algowokrs.com>
2024-04-09 19:15:27 +0530

    added mui library, and added code for responsive header(landing page), also added few more files and folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { createTheme, colors, ThemeProvider } from "@mui/material";
+import "./App.css";
+import Header from "./components/Header";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2e813e",
+      contrastText: "#fff",
+    },
+    primary_2: {
+      main: "#54585a",
+    },
+    secondary: {
+      main: "#f26d04",
+    },
+    text: {
+      primary: "#333333",
+      secondary: "#757575",
+    },
+  },
+});
 
 function App() {
   return (
-    <div>
-      Enerva EPP AM & V.
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Header />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,202 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import Drawer from "@mui/material/Drawer";
+import logo from "../assets/images/logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const logoStyle = {
+  width: "100px",
+  height: "auto",
+  cursor: "pointer",
+};
+
+function Header() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const scrollToSection = (sectionId) => {
+    const sectionElement = document.getElementById(sectionId);
+    const offset = 128;
+    if (sectionElement) {
+      const targetScroll = sectionElement.offsetTop - offset;
+      sectionElement.scrollIntoView({ behavior: "smooth" });
+      window.scrollTo({
+        top: targetScroll,
+        behavior: "smooth",
+      });
+      setOpen(false);
+    }
+  };
+
+  return (
+    <div>
+      <AppBar
+        position="fixed"
+        sx={{
+          boxShadow: 0,
+          bgcolor: "transparent",
+          backgroundImage: "none",
+          mt: 2,
+        }}
+      >
+        <Container maxWidth="lg">
+          <Toolbar
+            variant="regular"
+            sx={() => ({
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              flexShrink: 0,
+              backdropFilter: "blur(24px)",
+              maxHeight: 40,
+              borderColor: "divider",
+            })}
+          >
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: "flex",
+                alignItems: "center",
+                ml: "-18px",
+                px: 0,
+              }}
+            >
+              <img src={logo} style={logoStyle} alt="logo" />
+              <Box
+                sx={{
+                  display: { xs: "none", md: "flex" },
+                  flexGrow: 1,
+
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <MenuItem
+                  onClick={() => scrollToSection("features")}
+                  sx={{ py: "6px", px: "12px" }}
+                >
+                  <Typography variant="body2" color="text.primary">
+                    How it works
+                  </Typography>
+                </MenuItem>
+                <MenuItem
+                  onClick={() => scrollToSection("testimonials")}
+                  sx={{ py: "6px", px: "12px" }}
+                >
+                  <Typography variant="body2" color="text.primary">
+                    News Feed
+                  </Typography>
+                </MenuItem>
+                <MenuItem
+                  onClick={() => scrollToSection("highlights")}
+                  sx={{ py: "6px", px: "12px" }}
+                >
+                  <Typography variant="body2" color="text.primary">
+                    Contact Us
+                  </Typography>
+                </MenuItem>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                display: { xs: "none", md: "flex" },
+                gap: 0.5,
+                alignItems: "center",
+              }}
+            >
+              <Button
+                color="primary"
+                variant="text"
+                size="medium"
+                component="a"
+                href="/sign-in"
+                target="_blank"
+              >
+                Sign in
+              </Button>
+              <Button
+                color="primary"
+                variant="contained"
+                size="medium"
+                component="a"
+                href="/sign-up"
+                target="_blank"
+                sx={{ borderRadius: "0px" }}
+              >
+                Sign up
+              </Button>
+            </Box>
+            <Box sx={{ display: { sm: "", md: "none" } }}>
+              <Button
+                variant="text"
+                color="primary"
+                aria-label="menu"
+                onClick={toggleDrawer(true)}
+                sx={{ minWidth: "30px", p: "4px" }}
+              >
+                <MenuIcon />
+              </Button>
+              <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+                <Box
+                  sx={{
+                    minWidth: "60dvw",
+                    p: 2,
+                    backgroundColor: "background.paper",
+                    flexGrow: 1,
+                  }}
+                >
+                  <MenuItem onClick={() => scrollToSection("features")}>
+                    How it works
+                  </MenuItem>
+                  <MenuItem onClick={() => scrollToSection("testimonials")}>
+                    News Feed
+                  </MenuItem>
+                  <MenuItem onClick={() => scrollToSection("highlights")}>
+                    Contact Us
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem>
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      component="a"
+                      href="/sign-up"
+                      sx={{ width: "100%" }}
+                      target="_blank"
+                    >
+                      Sign up
+                    </Button>
+                  </MenuItem>
+                  <MenuItem>
+                    <Button
+                      color="primary"
+                      variant="outlined"
+                      component="a"
+                      href="/sign-in"
+                      target="_blank"
+                      sx={{ width: "100%" }}
+                    >
+                      Sign in
+                    </Button>
+                  </MenuItem>
+                </Box>
+              </Drawer>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;
