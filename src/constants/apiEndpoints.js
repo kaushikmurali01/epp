f78bbe1e12a3e09a518fc5ecd9d0ce9b469b1021
Avinash Tiwari <avinash.tiwari@algoworks.com>
2024-04-15 11:15:46 +0530

    Utils added with constant and theme.

---
+++ src/constants/apiEndpoints.js
@@ -0,0 +1,11 @@
+export const authEndpoints = {
+    login: '/login',
+    signup: '/sign-up',
+    forgetPassword: '/forget-password',
+    resetPassword: '/reset-password',
+    otpVerification: '/otp-verification'
+  };
+  
+  export const dashboardEndpoints = {
+    Profile: '/profile',
+  }
