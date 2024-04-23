3be972a2a1c4bb38944ee94f5fd5ab8eea50e8e4
mohitRaw <mohit.rawat@algowokrs.com>
2024-04-23 15:23:45 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = (props) => {
         {/* <ActionComponent /> */}
         {/* <Login /> */}
         {/* {/* <Signup /> */}
-        <LandingPage /> */}
+        <LandingPage />
         <Facility />
         <Footer />
       </ThemeProvider>
