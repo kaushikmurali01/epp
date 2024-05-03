7a860435d1a4e09f43ae62b84c82743d2eb4e2d8
Taran-Algo <taranpreet.singh@algoworks.com>
2024-05-03 12:01:27 +0530

    removal the user managemet

--- User Management/src/index.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-import dotenv from 'dotenv';
-
-const envFilePath = path.resolve(process.cwd(), '.env');
-dotenv.config({ path: envFilePath });
-console.log(process.env.PGHOST);
