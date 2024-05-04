275e029a3d819344503e4a59a790442be89c1f1b
mohitRaw <mohit.rawat@algowokrs.com>
2024-05-04 20:52:25 +0530

    updated code for api implimentation

--- src/constants/apiEndPoints.js
+++ src/constants/apiEndPoints.js
@@ -2,6 +2,8 @@ export const facilityEndPoints = {
   ADD_EDIT_FACILITY: "/company-facility/v1/facility",
   FACILITY_LIST: "/company-facility/v1/facility-listing",
   GET_FACILITY_BY_ID: "/company-facility/v1/facility-details",
+  SUBMIT_FACILITY_FOR_APPROVAL: "/company-facility/v1/facility-submission",
+  GET_FACILITY_DETAILS: "/company-facility/v1/facility-details",
 };
 
 export const meterEndPoints = {
