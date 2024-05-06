65dfe8b538d7a62ed06fbeaf45f157a131293c83
Avinash Tiwari <avinash.tiwari@algoworks.com>
2024-05-06 15:10:15 +0530

    image upload issue resolved

--- src/redux/actions/fileUploadAction.js
+++ src/redux/actions/fileUploadAction.js
@@ -7,14 +7,13 @@ import {
 import { POST_REQUEST } from "utils/HTTPRequests";
 
 export const fileUploadAction = (uploadData) => {
-  console.log(uploadData);
   return async (dispatch) => {
     try {
       dispatch(fileUploadRequest());
       const formData = new FormData();
-      formData.append("file", uploadData);
+      formData.append('file', uploadData);
       const endpoint = fileUploadEndPoints.FILE_UPLOAD;
-      const response = await POST_REQUEST(endpoint, formData, true);
+      const response = await POST_REQUEST(endpoint, formData, true, '');
       const data = response.data;
       dispatch(fileUploadSuccess(data));
       return data;
