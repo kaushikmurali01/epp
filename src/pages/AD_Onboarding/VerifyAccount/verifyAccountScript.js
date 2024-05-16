0c032a94ad7c471df2c913d51fcc7d17542cdc2f
Ashutosh Singh <ashutosh.singh@algoworks.com>
2024-05-16 19:50:11 +0530

    changes in Onboarding

--- src/pages/AD_Onboarding/VerifyAccount/verifyAccountScript.js
+++ src/pages/AD_Onboarding/VerifyAccount/verifyAccountScript.js
@@ -1,7 +1,1 @@
-document.getElementById("ReadOnlyEmail").style.borderColor = "green";
-document
-  .getElementById("ReadOnlyEmail_ver_but_send")
-  .classList.add("success-btn");
-  document
-    .getElementById("continue")
-    .classList.add("success-btn");
+document.getElementById("cancel").classList.add("grey-btn");
