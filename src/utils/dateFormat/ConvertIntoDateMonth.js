291d942e629152fc76a048f4057f06451791758a
Avinash Tiwari <avinash.tiwari@algoworks.com>
2024-05-24 13:49:37 +0530

    date format changed in invitation popup

--- src/utils/dateFormat/ConvertIntoDateMonth.js
+++ src/utils/dateFormat/ConvertIntoDateMonth.js
@@ -20,7 +20,24 @@ const monthName = monthNames[monthIndex];
 
 // Construct the desired format
 const formattedDate = `${day}-${monthName.substring(0, 3)}`;
+return formattedDate;
+}
 
-    return formattedDate;
+export const parseUTCDateToLocalDate = (utcdate) => {
+  // Parse our locale string to [date, time]
+  let date = new Date(utcdate).toLocaleString({},{hour12:false}).split(" ");
 
-  }
+  // Now we can access our time at date[1], and monthdayyear @ date[0]
+  let time = date[1];
+  let mdy = date[0];
+
+  // We then parse  the mdy into parts
+  mdy = mdy.split('/');
+  let month = String(+parseInt(mdy[0])).charAt(0) == parseInt(mdy[0]) ? "0"+parseInt(mdy[0]) : parseInt(mdy[0]) 
+  let day = String(+parseInt(mdy[1])).charAt(0) == parseInt(mdy[1]) ? "0"+parseInt(mdy[1]) : parseInt(mdy[1])    ;
+  let year = parseInt(mdy[2]);
+
+  // Putting it all together
+  let formattedDate = year + '-' + month + '-' + day + ' ' + time;
+  return formattedDate
+}
