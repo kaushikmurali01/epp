04881f04e3f836ac6e645376f5e3ce723fb20889
ujjawal goel <ujjawal.goel@algoworks.com>
2024-06-19 10:00:54 +0530

    add facility ubi oce

--- facility_managment/src/models/facility.model.ts
+++ facility_managment/src/models/facility.model.ts
@@ -1,229 +1,227 @@
-import { DataTypes, Model, Optional } from 'sequelize';
-import { sequelize } from '../utils/database';
-import { IFacilityAttributes } from '../interfaces/facility.interface';
-import { User } from './user.model';
-import { Company } from './company.model';
+import { DataTypes, Model, Optional } from "sequelize";
+import { sequelize } from "../utils/database";
+import { IFacilityAttributes } from "../interfaces/facility.interface";
+import { User } from "./user.model";
+import { Company } from "./company.model";
 
+interface FacilityCreationAttributes
+  extends Optional<IFacilityAttributes, "id"> {}
 
-interface FacilityCreationAttributes extends Optional<IFacilityAttributes, 'id'> {}
+class Facility
+  extends Model<IFacilityAttributes, FacilityCreationAttributes>
+  implements IFacilityAttributes
+{
+  public id!: number;
+  public facility_construction_status!: number;
+  public facility_name!: string;
+  public naic_code!: string;
+  public facility_category!: string;
+  public facility_type!: string;
+  public target_saving!: string;
+  public display_pic_url: string;
+  public unit_number!: string;
+  public street_number!: string;
+  public street_name!: string;
+  public city!: string;
+  public country!: string;
+  public province!: string;
+  public postal_code!: string;
+  public address!: string;
+  public sector!: string;
+  public year_of_construction!: Date;
+  public gross_floor_area!: string;
+  public number_of_storeys!: number;
+  public occupancy!: number;
+  public number_of_building!: number;
+  public company_id!: number;
+  public facility_id_general_status!: number;
+  public facility_id_submission_status!: number;
+  public ng_distribution_company!: string;
+  public ng_distribution_company_data_extraction!: string;
+  public longitude!: number;
+  public latitude!: number;
+  public facility_bas!: string;
+  public facility_bas_connectivity!: boolean;
+  public is_approved!: boolean;
+  public approved_by!: number;
+  public is_active!: number;
+  public created_at!: Date;
+  public updated_at!: Date;
+  public created_by!: number;
+  public updated_by!: number;
+  public facility_ubi!: string;
 
-class Facility extends Model<IFacilityAttributes, FacilityCreationAttributes> implements IFacilityAttributes  {
-    public id!: number;
-    public facility_construction_status!:number
-    public facility_name!: string;
-    public naic_code!: string;
-    public facility_category!: string;
-    public facility_type!: string;
-    public target_saving!: string;
-    public display_pic_url: string;
-    public unit_number !: string;
-    public street_number !: string;
-    public street_name !: string;
-	public city !: string;
-	public country !: string;
-	public province !: string;
-    public postal_code !: string;
-    public address !: string;
-    public sector !: string;
-    public year_of_construction !: Date;
-    public gross_floor_area !: string;
-    public number_of_storeys !: number;
-    public occupancy !: number;
-    public number_of_building !: number;
-    public company_id !: number;
-    public facility_id_general_status !: number;
-	public facility_id_submission_status !: number;
-	public ng_distribution_company!: string;
-	public ng_distribution_company_data_extraction!: string;
-    public longitude!: number;
-    public latitude!: number;
-	public facility_bas!: string;
-	public facility_bas_connectivity!: boolean;
-	public is_approved!: boolean;
-    public approved_by!: number;
-	public is_active!: number;
-	public created_at!: Date;
-	public updated_at!: Date;
-	public created_by!: number;
-	public updated_by!: number;
-
-    public total_electricity_savings!: number;
-    public energy_savings!: number;
-    public total_incentive_earned!: number;
-    public benchmarking_eui!: number;
-    public facility_nickname!: string;
-
+  public total_electricity_savings!: number;
+  public energy_savings!: number;
+  public total_incentive_earned!: number;
+  public benchmarking_eui!: number;
+  public facility_nickname!: string;
 }
-    
 
 /**
  * Facility model definition.
- * 
+ *
  * Defines the structure and constraints for the 'Facilitys' table in the database.
- * 
+ *
  */
 
 Facility.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        facility_construction_status: {
-            type: DataTypes.INTEGER,
-        },
-        facility_name: {
-            type: DataTypes.STRING(255),
-        },
-        naic_code: {
-            type: DataTypes.STRING(255),
-        },
-        facility_category: {
-            type: DataTypes.STRING(255),
-        },
-        facility_type: {
-            type: DataTypes.STRING(255),
-        },
-        target_saving: {
-            type: DataTypes.STRING(255),
-        },
-        display_pic_url: {
-            type: DataTypes.STRING(255),
-        },
-        unit_number: {
-            type: DataTypes.STRING(255),
-        },
-        street_number: {
-            type: DataTypes.STRING(255),
-        },
-        street_name: {
-            type: DataTypes.STRING(255),
-        },
-        city: {
-            type: DataTypes.STRING(255),
-        },
-        country: {
-            type: DataTypes.STRING(255),
-        },
-        province: {
-            type: DataTypes.STRING(255),
-        },
-        postal_code: {
-            type: DataTypes.STRING(255),
-        },
-        address: {
-            type: DataTypes.STRING(255),
-        },
-        sector: {
-            type: DataTypes.STRING(255),
-        },
-        year_of_construction: {
-            type: DataTypes.DATE,
-        },
-        gross_floor_area: {
-            type: DataTypes.STRING(255),
-        },
-        number_of_storeys: {
-            type: DataTypes.INTEGER,
-        },
-        occupancy: {
-            type: DataTypes.INTEGER,
-        },
-        number_of_building: {
-            type: DataTypes.INTEGER,
-        },
-        company_id: {
-            type: DataTypes.INTEGER,
-        },
-        facility_id_general_status: {
-            type: DataTypes.INTEGER,
-        },
-        facility_id_submission_status: {
-            type: DataTypes.INTEGER,
-        },
-        ng_distribution_company: {
-            type: DataTypes.STRING(255),
-        },
-        ng_distribution_company_data_extraction: {
-            type: DataTypes.TEXT,
-        },
-        longitude: {
-            type: DataTypes.DECIMAL,
-        },
-        latitude: {
-            type: DataTypes.DECIMAL,
-        },
-        facility_bas: {
-            type: DataTypes.TEXT,
-        },
-        facility_bas_connectivity: {
-            type: DataTypes.BOOLEAN,
-        },
-        is_approved: {
-            type: DataTypes.BOOLEAN,
-        },
-        approved_by: {
-            type: DataTypes.INTEGER,
-        },
-        is_active: {
-            type: DataTypes.INTEGER,
-        },
-        created_at: {
-            type: DataTypes.DATE,
-        },
-        updated_at: {
-            type: DataTypes.DATE,
-        },
-        created_by: {
-            type: DataTypes.INTEGER,
-        },
-        updated_by: {
-            type: DataTypes.INTEGER,
-        },
-
-        total_electricity_savings: {
-            type: DataTypes.INTEGER,
-        },
-        energy_savings: {
-            type: DataTypes.INTEGER,
-        },
-        total_incentive_earned: {
-            type: DataTypes.INTEGER,
-        },
-        benchmarking_eui: {
-            type: DataTypes.INTEGER,
-        },
-        facility_nickname: {
-            type: DataTypes.STRING(255)
-        }
-
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        sequelize,
-        tableName: 'facility', 
-        timestamps: true,
-        underscored: true, 
-        modelName: 'Facility', 
-    }
+    facility_construction_status: {
+      type: DataTypes.INTEGER,
+    },
+    facility_name: {
+      type: DataTypes.STRING(255),
+    },
+    naic_code: {
+      type: DataTypes.STRING(255),
+    },
+    facility_category: {
+      type: DataTypes.STRING(255),
+    },
+    facility_type: {
+      type: DataTypes.STRING(255),
+    },
+    target_saving: {
+      type: DataTypes.STRING(255),
+    },
+    display_pic_url: {
+      type: DataTypes.STRING(255),
+    },
+    unit_number: {
+      type: DataTypes.STRING(255),
+    },
+    street_number: {
+      type: DataTypes.STRING(255),
+    },
+    street_name: {
+      type: DataTypes.STRING(255),
+    },
+    city: {
+      type: DataTypes.STRING(255),
+    },
+    country: {
+      type: DataTypes.STRING(255),
+    },
+    province: {
+      type: DataTypes.STRING(255),
+    },
+    postal_code: {
+      type: DataTypes.STRING(255),
+    },
+    address: {
+      type: DataTypes.STRING(255),
+    },
+    sector: {
+      type: DataTypes.STRING(255),
+    },
+    year_of_construction: {
+      type: DataTypes.DATE,
+    },
+    gross_floor_area: {
+      type: DataTypes.STRING(255),
+    },
+    number_of_storeys: {
+      type: DataTypes.INTEGER,
+    },
+    occupancy: {
+      type: DataTypes.INTEGER,
+    },
+    number_of_building: {
+      type: DataTypes.INTEGER,
+    },
+    company_id: {
+      type: DataTypes.INTEGER,
+    },
+    facility_id_general_status: {
+      type: DataTypes.INTEGER,
+    },
+    facility_id_submission_status: {
+      type: DataTypes.INTEGER,
+    },
+    ng_distribution_company: {
+      type: DataTypes.STRING(255),
+    },
+    ng_distribution_company_data_extraction: {
+      type: DataTypes.TEXT,
+    },
+    longitude: {
+      type: DataTypes.DECIMAL,
+    },
+    latitude: {
+      type: DataTypes.DECIMAL,
+    },
+    facility_bas: {
+      type: DataTypes.TEXT,
+    },
+    facility_bas_connectivity: {
+      type: DataTypes.BOOLEAN,
+    },
+    is_approved: {
+      type: DataTypes.BOOLEAN,
+    },
+    approved_by: {
+      type: DataTypes.INTEGER,
+    },
+    is_active: {
+      type: DataTypes.INTEGER,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+    },
+    created_by: {
+      type: DataTypes.INTEGER,
+    },
+    updated_by: {
+      type: DataTypes.INTEGER,
+    },
+    facility_ubi: {
+      type: DataTypes.STRING(255),
+    },
+    total_electricity_savings: {
+      type: DataTypes.INTEGER,
+    },
+    energy_savings: {
+      type: DataTypes.INTEGER,
+    },
+    total_incentive_earned: {
+      type: DataTypes.INTEGER,
+    },
+    benchmarking_eui: {
+      type: DataTypes.INTEGER,
+    },
+    facility_nickname: {
+      type: DataTypes.STRING(255),
+    },
+  },
+  {
+    sequelize,
+    tableName: "facility",
+    timestamps: true,
+    underscored: true,
+    modelName: "Facility",
+  }
 );
 
-
 Facility.belongsTo(User, {
-    foreignKey: 'created_by',
-    as: 'submitted_by',
-  });
-  
-  Facility.belongsTo(Company, {
-    foreignKey: 'company_id',
-    as: 'company',
-  });
+  foreignKey: "created_by",
+  as: "submitted_by",
+});
 
+Facility.belongsTo(Company, {
+  foreignKey: "company_id",
+  as: "company",
+});
 
 export { Facility };
-
-
-
 
 // Facility.init(
 //     {
