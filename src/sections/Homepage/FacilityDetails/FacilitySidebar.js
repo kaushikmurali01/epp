1c8191360705a2a50d017cc2fe0f7cb499f22e21
Ashutosh Singh <ashutosh.singh@algoworks.com>
2024-06-20 13:15:08 +0530

    conflict removed

--- src/sections/Homepage/FacilityDetails/FacilitySidebar.js
+++ src/sections/Homepage/FacilityDetails/FacilitySidebar.js
@@ -16,10 +16,10 @@ export default function FacilitySidebar({ selectedTab, setSelectedTab }) {
       type: "input",
       value: "energy_and_water",
     },
-    { label: "Weather & Independent Variables", id: 3, type: "input" , value:"weather"},
-    { label: "Reports and Studies", id: 4, type: "input", value:"reports_and_studies" },
-    { label: "Baseline Model", id: 5, type: "input", value:"baseline_model" },
-    { label: "Performance", id: 6, type: "input", value:"performance" },
+    // { label: "Weather & Independent Variables", id: 3, type: "input" , value:"weather"},
+    // { label: "Reports and Studies", id: 4, type: "input", value:"reports_and_studies" },
+    // { label: "Baseline Model", id: 5, type: "input", value:"baseline_model" },
+    // { label: "Performance", id: 6, type: "input", value:"performance" },
   ];
 
   const handleTabChange = (event, newValue) => {
