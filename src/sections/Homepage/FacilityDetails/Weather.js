66f22097e82973615bc4c614b6fd48d7ce0ef97e
Avinash Tiwari <avinash.tiwari@algoworks.com>
2024-06-28 19:05:02 +0530

    weather visualisation added

--- src/sections/Homepage/FacilityDetails/Weather.js
+++ src/sections/Homepage/FacilityDetails/Weather.js
@@ -18,6 +18,8 @@ import {
   FormControlLabel,
   Checkbox
 } from "@mui/material";
+import { PowerBIEmbed } from "powerbi-client-react";
+import { models } from "powerbi-client";
 import { useSelector } from "react-redux";
 import { GET_REQUEST, POST_REQUEST } from "utils/HTTPRequests";
 import InputField from "components/FormBuilder/InputField";
@@ -32,6 +34,8 @@ import { useDispatch } from "react-redux";
 import { documentFileUploadAction } from "../../../redux/global/actions/fileUploadAction";
 import { WEATHER_INDEPENDENT_VARIABLE_ENDPOINTS } from "constants/apiEndPoints";
 import NotificationsToast from "utils/notification/NotificationsToast";
+import { POWERBI_POST_REQUEST } from "utils/powerBiHttpRequests";
+import { POWERBI_ENDPOINTS } from "constants/apiEndPoints";
 
 const Weather = () => {
   const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("md"));
@@ -42,6 +46,12 @@ const Weather = () => {
   const facilityData = useSelector(
     (state) => state?.facilityReducer?.facilityDetails?.data
   );
+  const [isErrorInPowerBi, setIsErrorInPowerBi] = useState(false)
+
+  const weatherDataSetId = process.env.REACT_APP_POWERBI_WEATHER_DATASET_ID
+  const weatherReportId = process.env.REACT_APP_POWERBI_WEATHER_REPORT_ID
+  const weatherEmbedUrl = process.env.REACT_APP_POWERBI_WEATHER_EMBED_URL
+  const [reportLoading, setReportLoading] = useState(true)
   const [tabValue, setTabValue] = useState("weather");
   const [isFileUploaded, setIsFileUploaded] = useState(false);
   const [independentVariable1File, setIndependentVariable1File] = useState(null);
@@ -71,6 +81,92 @@ const Weather = () => {
       station_id: "51459"
     }
   ];
+
+  useEffect(() => {
+    if(!facilityData) return;
+    getPowerBiToken()
+  }, [facilityData])
+
+  const getPowerBiToken = () => {
+    const apiURL = POWERBI_ENDPOINTS.GET_AZURE_TOKEN_FOR_POWER_BI
+
+    GET_REQUEST(apiURL).then((response) => {
+      localStorage.setItem("powerBiAccessToken", (response?.data?.access_token));
+      getPowerBiReportToken()
+    })
+    .catch((error) => {
+      console.log(error);
+      if(error?.response?.status == 403){
+      }
+      setReportLoading(false);
+    });
+  }
+
+  const getPowerBiReportToken= () => {
+    setReportLoading(true)
+    const apiURL = POWERBI_ENDPOINTS.GET_POWERBI_TOKEN;
+    const body = {
+      "datasets": [
+        {
+          "id": weatherDataSetId
+        }
+      ],
+      "reports": [
+        {
+          "allowEdit": true,
+          "id": weatherReportId
+        }
+      ]
+    }
+    POWERBI_POST_REQUEST(apiURL, body)
+      .then((res) => {
+        localStorage.setItem("powerBiReportToken", JSON.stringify(res?.data))
+        setReportParameters();
+      })
+      .catch((error) => {
+        console.log(error);
+        if(error?.response?.status == 403){
+        }
+        setReportLoading(false);
+      });
+  }
+
+  let powerBiReportToken = localStorage.getItem("powerBiReportToken") ? JSON.parse(localStorage.getItem("powerBiReportToken")) : null;
+
+  const setReportParameters = () => {
+    const apiURL = `https://api.powerbi.com/v1.0/myorg/groups/d5ca9c18-0e45-4f7a-8b5a-0e0c75ddec73/datasets/${weatherDataSetId}/Default.UpdateParameters`
+    const body = {
+      "updateDetails": [
+        {
+          "name": "facility_id",
+          "newValue": facilityData?.id
+        },
+      ]
+    }
+    POWERBI_POST_REQUEST(apiURL, body)
+      .then((res) => {
+        refreshPowerBiReport()
+      })
+      .catch((error) => {
+        setReportLoading(false)
+        console.log(error);
+      });
+  }
+
+  const refreshPowerBiReport = () => {
+    const apiURL = `https://api.powerbi.com/v1.0/myorg/groups/d5ca9c18-0e45-4f7a-8b5a-0e0c75ddec73/datasets/${weatherDataSetId}/refreshes`
+    const body = {
+      retryCount: 3
+    }
+    POWERBI_POST_REQUEST(apiURL, body, )
+      .then((res) => {
+        setReportLoading(false)
+      })
+      .catch((error) => {
+        setReportLoading(false)
+        console.log(error);
+      });
+  }
 
   const [modalConfig, setModalConfig] = useState({
     modalVisible: false,
@@ -237,6 +333,30 @@ const Weather = () => {
   const selectedIv= independentVarsList?.length && independentVarsList.find(obj => obj['id'] == tabValue);
   const selectedIvName = selectedIv ? selectedIv['name'] : undefined;
 
+  const getPowerBiError = (errorDetail) => {
+    console.log('Error in setIsErrorInPowerBi',errorDetail)
+  }
+ 
+  let powerBiConfig = {
+    type: "report",
+    id: weatherReportId,
+    embedUrl: weatherEmbedUrl, 
+    accessToken: powerBiReportToken?.token || null,
+    tokenType: models.TokenType.Embed,
+    settings: {
+      panes: {
+        filters: {
+          expanded: false,
+          visible: false, // Hide the filter pane
+        },
+        pageNavigation: {
+          visible: false, // Hide the page navigation
+        },
+      },
+      background: models.BackgroundType.Transparent,
+    },
+  }
+
   console.log("ffffvvv", independentVariable1File)
 
   return (
@@ -313,7 +433,7 @@ const Weather = () => {
             marginBottom: "3rem",
           }}
         >
-          <Grid item xs={12} md={7} sx={{ textAlign: "center" }}>
+          <Grid item xs={12} md={12} sx={{ textAlign: "center" }}>
             <TableContainer
               component={Paper}
               sx={{
@@ -393,7 +513,7 @@ const Weather = () => {
               </MuiTable>
             </TableContainer>
           </Grid>
-          <Grid container item xs={12} md={5} sx={{ padding: " 0px 17px" }}>
+          {/* <Grid container item xs={12} md={5} sx={{ padding: " 0px 17px" }}>
             <Typography>Select checkboxes to see graphs</Typography>
             <Grid item xs={12} md={6}>
               <Formik
@@ -536,6 +656,55 @@ const Weather = () => {
                   </FormGroup>
                 </Form>
               </Formik>
+            </Grid>
+          </Grid> */}
+          <Grid sx={{width: "100%"}}>
+            <Grid sx={{width: "100%"}}>
+              <Box id="bi-report" mt={4}>
+                {((!isErrorInPowerBi && !reportLoading)) ? <PowerBIEmbed
+                  embedConfig={powerBiConfig}
+                  eventHandlers={
+                    new Map([
+                      [
+                        "loaded",
+                        function () {
+                          console.log("Report loaded");
+                        },
+                      ],
+                      [
+                        "rendered",
+                        function () {
+                          console.log("Report rendered");
+                        },
+                      ],
+                      [
+                        "error",
+                        function (event) {
+                          console.log("iiiiiiiiiii",event.detail);
+                          getPowerBiError(event.detail)
+                        },
+                      ],
+                      ["visualClicked", () => console.log("visual clicked")],
+                      ["pageChanged", (event) => console.log(event)],
+                    ])
+                  }
+                  cssClassName={"bi-embedded"}
+                  getEmbeddedComponent={(embeddedReport) => {
+                    window.report = embeddedReport;
+                  }}
+                /> : 
+                <Typography
+                  variant="h3"
+                  sx={{
+                    fontWeight: "700",
+                    fontSize: "1.125rem !important",
+                    lineHeight: "106.815%",
+                    letterSpacing: "-0.01125rem",
+                  }}
+                >
+                  Either data has not been uploaded and verified yet or uploaded data is in processing state, so this visualization is not available.
+                </Typography>}
+              </Box>
             </Grid>
           </Grid>
         </Grid>
