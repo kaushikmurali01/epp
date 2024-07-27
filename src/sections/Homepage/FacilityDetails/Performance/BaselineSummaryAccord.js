ec36758a947c4c893b0afc93c6303c9b06ca0950
ashutosh singh <ashutosh.singh@algoworks.com>
2024-07-27 22:43:22 +0530

    baseline summary update

--- src/sections/Homepage/FacilityDetails/Performance/BaselineSummaryAccord.js
+++ src/sections/Homepage/FacilityDetails/Performance/BaselineSummaryAccord.js
@@ -2,6 +2,9 @@ import { Grid, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getBaselineDataSummary } from "../../../../redux/superAdmin/actions/performanceAction";
+import { getIncentiveSettings } from "../../../../redux/admin/actions/adminPerformanceActions";
+import Loader from "pages/Loader";
+
 const baselineStyleInAccordion = {
   color: "#242424",
   padding: "0.375rem 1rem",
@@ -9,80 +12,105 @@ const baselineStyleInAccordion = {
   fontStyle: "normal",
   fontWeight: 500,
 };
-const BaselineSummaryAccord = () => {
-  const summaryData = useSelector(
-    (state) => state?.performanceReducer?.baselineSummaryData
+
+const BaselineSummaryAccord = ({ meter_type }) => {
+  const facility_id = useSelector(
+    (state) => state?.facilityReducer?.facilityDetails?.data?.id
+  );
+  const incentiveSettings = useSelector(
+    (state) => state?.adminPerformanceReducer?.incentiveSettings
+  );
+  const { baselineSummaryData, loading } = useSelector(
+    (state) => state?.performanceReducer
   );
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const summaryBody = {facility_id:24, meter_id:1}
+    const summaryBody = { facility_id: 336, meter_type };
     dispatch(getBaselineDataSummary(summaryBody));
-  }, [dispatch]);
-  
+    if (facility_id) {
+      dispatch(getIncentiveSettings(facility_id));
+    }
+  }, [dispatch, meter_type, facility_id]);
+
+  console.log(baselineSummaryData, incentiveSettings, "baselineSummaryData");
+
+  const baselineSummary =
+    baselineSummaryData?.baseline_summary_performance_page || {};
+
   return (
-    <Grid container display={"grid"}>
-      <Grid item>
-        <Typography
-          sx={{
-            color: "#2C77E9",
-            fontSize: "14px !important",
-            fontWeight: "500",
-            padding: "0.375rem 1rem",
-          }}
-        >
-          Baseline Energy Model
-        </Typography>
+    <>
+      <Grid container display={"grid"}>
+        <Grid item>
+          <Typography
+            sx={{
+              color: "#2C77E9",
+              fontSize: "14px !important",
+              fontWeight: "500",
+              padding: "0.375rem 1rem",
+            }}
+          >
+            Baseline Energy Model
+          </Typography>
+        </Grid>
+        <Grid item container>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6" sx={baselineStyleInAccordion}>
+              Baseline Periods
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Typography variant="h6" sx={baselineStyleInAccordion}>
+              {baselineSummary["Baseline Energy Periods"] || "N/A"}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item container>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6" sx={baselineStyleInAccordion}>
+              Baseline Energy Consumption (kWh)
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Typography variant="h6" sx={baselineStyleInAccordion}>
+              {baselineSummary["Baseline Energy Consumption"] || "N/A"}
+            </Typography>
+          </Grid>
+        </Grid>
+        {meter_type === 1 && (
+          <Grid item container>
+            <Grid item xs={12} md={4}>
+              <Typography variant="h6" sx={baselineStyleInAccordion}>
+                Baseline Peak Demand (kW)
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <Typography variant="h6" sx={baselineStyleInAccordion}>
+                {baselineSummary["Baseline Peak Demand"] || "N/A"}
+              </Typography>
+            </Grid>
+          </Grid>
+        )}
+        <Grid item container>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6" sx={baselineStyleInAccordion}>
+              Pre-Project Incentive ($)
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Typography variant="h6" sx={baselineStyleInAccordion}>
+              {incentiveSettings?.preProjectIncentive || "N/A"}
+            </Typography>
+          </Grid>
+        </Grid>
       </Grid>
-      <Grid item container>
-        <Grid item xs={12} md={4}>
-          <Typography variant="h6" sx={baselineStyleInAccordion}>
-            Baseline Periods
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <Typography variant="h6" sx={baselineStyleInAccordion}>
-            XXXX
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item container>
-        <Grid item xs={12} md={4}>
-          <Typography variant="h6" sx={baselineStyleInAccordion}>
-            Baseline Energy Consumption (kWh)
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <Typography variant="h6" sx={baselineStyleInAccordion}>
-            XXXX
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item container>
-        <Grid item xs={12} md={4}>
-          <Typography variant="h6" sx={baselineStyleInAccordion}>
-            Baseline Peak Demand (kW)
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <Typography variant="h6" sx={baselineStyleInAccordion}>
-            XXXX
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item container>
-        <Grid item xs={12} md={4}>
-          <Typography variant="h6" sx={baselineStyleInAccordion}>
-            Pre-Project Incentive ($)
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <Typography variant="h6" sx={baselineStyleInAccordion}>
-            XXXX
-          </Typography>
-        </Grid>
-      </Grid>
-    </Grid>
+      <Loader
+        sectionLoader
+        minHeight="100vh"
+        loadingState={loading}
+        loaderPosition="fixed"
+      />
+    </>
   );
 };
 
