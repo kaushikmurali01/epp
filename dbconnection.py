82914b9e7bd10d0e18f2570b83ae479a92339813
Varun Pratap Singh <varun.singh@algoworks.com>
2024-07-28 15:24:37 +0530

    Finalising Uploader Code

--- dbconnection.py
+++ dbconnection.py
@@ -1,67 +1,60 @@
-import psycopg2
-from sshtunnel import SSHTunnelForwarder
-import pandas as pd
 import psycopg2.extras
+
 import config
+import psycopg2
+import psycopg2.extras
+import pandas as pd
+from sshtunnel import SSHTunnelForwarder
+
+
+def get_db_connection():
+    if config.LOCAL:
+        server = SSHTunnelForwarder(
+            (config.ssh_ip, 22),
+            ssh_username=config.ssh_user,
+            ssh_private_key=config.private_key_path,
+            remote_bind_address=(config.ssh_bind_address, 5432)
+        )
+        server.start()
+        params = {
+            'database': config.db_creds[0],
+            'user': config.db_creds[1],
+            'password': config.db_creds[2],
+            'host': '127.0.0.1',  # Use localhost since we're using SSH tunneling
+            'port': server.local_bind_port
+        }
+        conn = psycopg2.connect(**params)
+        return conn#, server  # Return the server object for later stopping
+    else:
+        params = {
+            'database': config.db_creds[0],
+            'user': config.db_creds[1],
+            'password': config.db_creds[2],
+            'host': config.db_creds[3],
+            'port': config.port  # Assuming the database is directly accessible on this port
+        }
+        return psycopg2.connect(**params)
 
 
 def dbtest(query):
-    with SSHTunnelForwarder(
-            (config.ssh_ip, 22),
-            ssh_username=config.ssh_user,
-            ssh_private_key=config.private_key_path,
-            #    ssh_password=config.ssh_password,  # Assuming you use a password
-            remote_bind_address=(config.ssh_bind_address, 5432)
-    ) as server:
-        server.start()
-        params = {
-            'database': config.db_creds[0],
-            'user': config.db_creds[1],
-            'password': config.db_creds[2],
-            'host': config.db_creds[3],
-            'port': server.local_bind_port
-        }
-
-        conn = psycopg2.connect(**params)
-        curs = conn.cursor()
-        curs.execute(query)
-        rows = curs.fetchall()
-        colnames = [desc[0] for desc in curs.description]
-        df = pd.DataFrame(rows, columns=colnames)
-
-        curs.close()
-        conn.close()
-
-        return df
+    conn = get_db_connection()
+    curs = conn.cursor()
+    curs.execute(query)
+    rows = curs.fetchall()
+    colnames = [desc[0] for desc in curs.description]
+    df = pd.DataFrame(rows, columns=colnames)
+    curs.close()
+    conn.close()
+    return df
 
 
 def db_execute(query, values):
-    with SSHTunnelForwarder(
-            (config.ssh_ip, 22),
-            ssh_private_key=config.private_key_path,
-            ssh_username=config.ssh_user,
-            remote_bind_address=(config.ssh_bind_address, 5432)
-    ) as server:
-        server.start()
-        # print("server connected")
-
-        params = {
-            'database': config.db_creds[0],
-            'user': config.db_creds[1],
-            'password': config.db_creds[2],
-            'host': config.db_creds[3],
-            'port': server.local_bind_port
-        }
-
-        conn = psycopg2.connect(**params)
-        curs = conn.cursor()
-        # print("database connected")
-
-        psycopg2.extras.execute_batch(curs, query, values)
-        conn.commit()
-        curs.close()
-        conn.close()
-        # print("Data inserted successfully")
+    conn = get_db_connection()
+    curs = conn.cursor()
+    psycopg2.extras.execute_batch(curs, query, values)
+    conn.commit()
+    curs.close()
+    conn.close()
 
 
 def fetch_nearest_station_data(target_latitude, target_longitude):
@@ -81,7 +74,7 @@ def fetch_nearest_station_data(target_latitude, target_longitude):
     ORDER BY distance
     LIMIT 1;
     """
-    conn = dbtest()
+    conn = get_db_connection()
     df = pd.read_sql_query(query, conn)
     conn.close()
     return df
@@ -106,176 +99,47 @@ def fetch_additional_stations_data(target_latitude, target_longitude):
     OFFSET 1
     LIMIT 2;
     """
-    conn = dbtest()
+    conn = get_db_connection()
     df = pd.read_sql_query(query, conn)
     conn.close()
     return df
 
 
-# def db_execute_single(query, values):
-#     query_with_returning = query + " RETURNING id"
-#     with SSHTunnelForwarder(
-#             (config.ssh_ip, 22),
-#             ssh_private_key=config.private_key_path,
-#             ssh_username=config.ssh_user,
-#             remote_bind_address=(config.ssh_bind_address, 5432)
-#     ) as server:
-#         server.start()
-#
-#     params = {
-#         'database': config.db_creds[0],
-#         'user': config.db_creds[1],
-#         'password': config.db_creds[2],
-#         'host': config.db_creds[3],
-#         'port': server.local_bind_port
-#     }
-#
-#     conn = psycopg2.connect(**params)
-#     curs = conn.cursor()
-#
-#     # Execute the query and fetch the generated ID
-#     print(query_with_returning, '\n\n\n',values)
-#     curs.execute(query_with_returning, values)
-#     generated_id = curs.fetchone()[0]  # Fetch the first (and only) row and get the ID
-#
-#     conn.commit()
-#     curs.close()
-#     conn.close()
-#
-#     return generated_id
 def db_execute_single(query, values):
     query_with_returning = query + " RETURNING id"
+    conn = get_db_connection()
+    curs = conn.cursor()
 
-    # Initialize and start the SSH tunnel
-    with SSHTunnelForwarder(
-            (config.ssh_ip, 22),
-            ssh_private_key=config.private_key_path,
-            ssh_username=config.ssh_user,
-            remote_bind_address=(config.ssh_bind_address, 5432)
-    ) as server:
-        server.start()  # Ensure the server is started before accessing local_bind_port
-
-        # Setup database connection parameters
-        params = {
-            'database': config.db_creds[0],
-            'user': config.db_creds[1],
-            'password': config.db_creds[2],
-            'host': '127.0.0.1',  # Use local host since we're using SSH tunneling
-            'port': server.local_bind_port
-        }
-
-        # Connect to the PostgreSQL database
-        conn = psycopg2.connect(**params)
-        curs = conn.cursor()
-
-        try:
-            # Execute the query and fetch the generated ID
-            curs.execute(query_with_returning, values)
-            generated_id = curs.fetchone()[0]  # Fetch the first (and only) row and get the ID
-
-            conn.commit()
-        except Exception as e:
-            conn.rollback()
-            print(f"Error executing query: {e}")
-            generated_id = None
-        finally:
-            curs.close()
-            conn.close()
+    try:
+        curs.execute(query_with_returning, values)
+        generated_id = curs.fetchone()[0]
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(f"Error executing query: {e}")
+        generated_id = None
+    finally:
+        curs.close()
+        conn.close()
 
     return generated_id
 
 
-# ToDO Need to improve this function to insert data into table using hourly entry file
-# def bulk_insert_df(df, table_name, record_id, file_table):
-#     # Generate the column names and the values placeholders
-#     cols = ','.join(list(df.columns))
-#     vals_placeholder = ','.join(['%s' for _ in df.columns])
-#
-#     # Generate the SQL query
-#     query = f"INSERT INTO {table_name} ({cols}) VALUES ({vals_placeholder})"
-#
-#     # Convert DataFrame to list of tuples using itertuples, excluding the index
-#     tuples = list(df.itertuples(index=False, name=None))
-#
-#     # Define the db_execute function
-#     def db_execute(query, values):
-#         with SSHTunnelForwarder(
-#                 (config.ssh_ip, 22),
-#                 ssh_private_key=config.private_key_path,
-#                 ssh_username=config.ssh_user,
-#                 remote_bind_address=(config.ssh_bind_address, 5432)
-#         ) as server:
-#             server.start()
-#             # print("server connected")
-#
-#             params = {
-#                 'database': config.db_creds[0],
-#                 'user': config.db_creds[1],
-#                 'password': config.db_creds[2],
-#                 'host': config.db_creds[3],
-#                 'port': server.local_bind_port
-#             }
-#
-#             conn = psycopg2.connect(**params)
-#             curs = conn.cursor()
-#             # print("database connected")
-#
-#             psycopg2.extras.execute_batch(curs, query, values)
-#             conn.commit()
-#             curs.close()
-#             conn.close()
-#
-#     # Execute the bulk insert
-#     db_execute(query, tuples)
-
-
 def bulk_insert_df(df, table_name, record_id, file_table):
-    # Generate the column names and the values placeholders
     cols = ','.join(list(df.columns))
     vals_placeholder = ','.join(['%s' for _ in df.columns])
-
-    # Generate the SQL query for inserting data
     insert_query = f"INSERT INTO {table_name} ({cols}) VALUES ({vals_placeholder})"
-
-    # Generate the SQL query for updating the flag
     update_query = f"UPDATE {file_table} SET processed = true WHERE id = %s"
-
-    # Convert DataFrame to list of tuples using itertuples, excluding the index
     tuples = list(df.itertuples(index=False, name=None))
 
-    # Define the db_execute function
     def db_execute(insert_query, update_query, insert_values, update_value):
-        with SSHTunnelForwarder(
-                (config.ssh_ip, 22),
-                ssh_private_key=config.private_key_path,
-                ssh_username=config.ssh_user,
-                remote_bind_address=(config.ssh_bind_address, 5432)
-        ) as server:
-            server.start()
-            # print("server connected")
+        conn = get_db_connection()
+        curs = conn.cursor()
+        psycopg2.extras.execute_batch(curs, insert_query, insert_values)
+        conn.commit()
+        curs.execute(update_query, (update_value,))
+        conn.commit()
+        curs.close()
+        conn.close()
 
-            params = {
-                'database': config.db_creds[0],
-                'user': config.db_creds[1],
-                'password': config.db_creds[2],
-                'host': config.db_creds[3],
-                'port': server.local_bind_port
-            }
-
-            conn = psycopg2.connect(**params)
-            curs = conn.cursor()
-            # print("database connected")
-
-            # Execute the bulk insert
-            psycopg2.extras.execute_batch(curs, insert_query, insert_values)
-            conn.commit()
-
-            # Execute the update query
-            curs.execute(update_query, (update_value,))
-            conn.commit()
-
-            curs.close()
-            conn.close()
-
-    # Execute the bulk insert and update
     db_execute(insert_query, update_query, tuples, record_id)
