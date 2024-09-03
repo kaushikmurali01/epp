dab00a7aefc07ae4882a9d704f446a0cd80c8664
Sarvat <sarvat.khan@algoworks.com>
2024-09-03 17:38:17 +0530

    staging user branch

--- user_management/src/middleware/authMiddleware.ts
+++ user_management/src/middleware/authMiddleware.ts
@@ -5,7 +5,7 @@ import { User } from "../models/user";
 import { UserCompanyRole } from "../models/user-company-role";
 import { sequelize } from "../services/database";
 
-// Middleware to decode JWT token
+// Middleware to decode JWT token.
 export async function decodeTokenMiddleware(req: HttpRequest, context: InvocationContext, next: () => Promise<HttpResponseInit>): Promise<any> {
     try {
         const token = req.headers.get('Authorization');
