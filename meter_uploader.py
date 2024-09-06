e937d3d6367fdc3c6e5b2b445c25d35d88ea46a7
Varun Pratap Singh <varun.singh@algoworks.com>
2024-09-06 06:11:32 +0530

    Invalid Sheets Check

--- meter_uploader.py
+++ meter_uploader.py
@@ -46,10 +46,20 @@ class Validators:
             "active_range": self.validate_data_within_meter_active_range(data_chunk=chunk)
         }
 
+    def validate_iv_chunk(self, chunk, iv, meter_id):
+        return {
+            # "time_diff": self.validate_time_difference(data_chunk=chunk),
+            "required_cols": self.validate_required_columns(data_chunk=chunk),
+            "start_end_dates": self.validate_start_end_dates(data_chunk=chunk),
+            "no_overlap": self.validate_no_overlapping_dates(iv, meter_id, data_chunk=chunk),
+            # "active_range": self.validate_data_within_meter_active_range(data_chunk=chunk)
+        }
+
     def validate_and_read_excel_sheets(self, excel_file, chunk_size=10000):
         excel_io = io.BytesIO(excel_file.read())
         wb = openpyxl.load_workbook(excel_io, read_only=True)
         visible_sheets = [sheet.title for sheet in wb.worksheets if sheet.sheet_state == 'visible']
+        invalid_sheets = []
         for sheet_name in visible_sheets:
             sheet = wb[sheet_name]
             rows = sheet.iter_rows(values_only=True)
@@ -59,19 +69,31 @@ class Validators:
                 chunk.append(row)
                 if len(chunk) == chunk_size:
                     df = pd.DataFrame(chunk, columns=header)
+                    available_cols = [col for col in df.columns if col in self.required_columns]
+                    if len(available_cols) != len(self.required_columns):
+                        invalid_sheets.append(sheet_name)
+                        continue
                     df = df[self.required_columns]
+
                     df['Start Date (Required)'] = pd.to_datetime(df['Start Date (Required)'], errors='coerce')
                     df['End Date (Required)'] = pd.to_datetime(df['End Date (Required)'], errors='coerce')
                     yield df
                     chunk = []
             if chunk:
                 df = pd.DataFrame(chunk, columns=header)
+                available_cols = [col for col in df.columns if col in self.required_columns]
+                if len(available_cols) != len(self.required_columns):
+                    invalid_sheets.append(sheet_name)
+                    continue
                 df = df[self.required_columns]
+
                 df['Start Date (Required)'] = pd.to_datetime(df['Start Date (Required)'], errors='coerce')
                 df['End Date (Required)'] = pd.to_datetime(df['End Date (Required)'], errors='coerce')
                 yield df
+
         wb.close()
-        # excel_io.close()
+        if invalid_sheets:
+            raise ValueError("Invalid Sheet(s):{}".format(','.join(invalid_sheets)))
         self.excel_io = excel_io
 
     def validate_no_overlapping_dates(self, iv, meter_id, data_chunk=None):
@@ -122,28 +144,66 @@ class MeterDataUploader(BaseDataUploader):
         return db_execute_single(insert_query_facility_meter_hourly_entries, values)
 
     def process(self):
-        validator = Validators(self.excel_file, self.meter_active, self.meter_inactive)
-        chunks = validator.validate_and_read_excel_sheets(self.excel_file)
+        try:
+            validator = Validators(self.excel_file, self.meter_active, self.meter_inactive)
+            chunks = validator.validate_and_read_excel_sheets(self.excel_file)
 
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            future_to_chunk = {executor.submit(validator.validate_chunk, chunk, self.iv, self.meter_id): chunk for chunk
-                               in chunks}
-            for future in concurrent.futures.as_completed(future_to_chunk):
-                try:
-                    result = future.result()
-                except Exception as exc:
-                    return {'success': False, 'error': str(exc)}
-        blob_name = generate_blob_name()
-        file_path = save_file_to_blob(blob_name, self.excel_file)
-        record_id = self.create_file_record_in_table(file_path)
-        print(self.excel_file)
-        return {
-            "success": True,
-            "message": "File Uploaded Successfully",
-            "path": file_path,
-            "record_id": record_id
-        }
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future_to_chunk = {executor.submit(validator.validate_chunk, chunk, self.iv, self.meter_id): chunk for
+                                   chunk
+                                   in chunks}
+                for future in concurrent.futures.as_completed(future_to_chunk):
+                    try:
+                        result = future.result()
+                    except Exception as exc:
+                        return {'success': False, 'error': str(exc)}
+            blob_name = generate_blob_name()
+            file_path = save_file_to_blob(blob_name, self.excel_file)
+            record_id = self.create_file_record_in_table(file_path)
+            return {
+                "success": True,
+                "message": "File Uploaded Successfully",
+                "path": file_path,
+                "record_id": record_id
+            }
+        except Exception as error:
+            return {
+                "success": False,
+                "error": "{}".format(error)
+            }
 
 
 class MeterDataUploaderIV:
-    pass
+
+    def create_file_record_in_table(self, file_path):
+        values = [self.meter_id, file_path]
+        return db_execute_single(insert_query_facility_iv_files_table, values)
+
+    def process(self):
+        try:
+            validator = Validators(self.excel_file, self.meter_active, self.meter_inactive)
+            chunks = validator.validate_and_read_excel_sheets(self.excel_file)
+
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future_to_chunk = {executor.submit(validator.validate_iv_chunk, chunk, self.iv, self.meter_id): chunk for
+                                   chunk
+                                   in chunks}
+                for future in concurrent.futures.as_completed(future_to_chunk):
+                    try:
+                        result = future.result()
+                    except Exception as exc:
+                        return {'success': False, 'error': str(exc)}
+            blob_name = generate_blob_name()
+            file_path = save_file_to_blob(blob_name, self.excel_file)
+            record_id = self.create_file_record_in_table(file_path)
+            return {
+                "success": True,
+                "message": "File Uploaded Successfully",
+                "path": file_path,
+                "record_id": record_id
+            }
+        except Exception as error:
+            return {
+                "success": False,
+                "error": "{}".format(error)
+            }
