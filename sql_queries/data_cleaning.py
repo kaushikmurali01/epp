f62ca54cd4648b65cd71c530a0fdb029686fd3c5
Varun Pratap Singh <varun.singh@algoworks.com>
2024-09-26 13:12:45 +0530

    Changes for Duplicate

--- sql_queries/data_cleaning.py
+++ sql_queries/data_cleaning.py
@@ -46,77 +46,8 @@ ORDER BY
     bd.meter_name;"""
 
 
-def get_data_cleaning_query(temp1, temp2, temp3, start_date, end_date, facility_id, meter_type):
-    # where_condition = f"AND meter_type = {meter_type}"
-    where_condition = f"AND (meter_type = {meter_type} OR is_independent_variable = true)"
-    # Convert tuple to SQL-friendly string format
-    # if independent_variables:
-    #     # Ensuring the format is '(value1, value2,...)'
-    #     independent_vars_formatted = str(independent_variables) if len(
-    #         independent_variables) > 1 else f"({independent_variables[0]})"
-    #     where_condition = f"AND (meter_type = {meter_type} OR independent_variable_id IN {independent_vars_formatted})"
-
-    return f"""
-    WITH base_data AS (
-        SELECT DATE_TRUNC('hour', start_date::timestamp) AS start_date,
-               CASE
-                   WHEN is_independent_variable THEN independent_variable_id
-                   ELSE meter_id
-               END AS meter_id,
-               CASE 
-                   WHEN meter_id = {temp1} THEN 'Temp1'
-                   WHEN meter_id = {temp2} THEN 'Temp2'
-                   WHEN meter_id = {temp3} THEN 'Temp3'
-                   ELSE meter_name 
-               END AS meter_name,
-               reading,
-               meter_type,
-               CASE 
-                   WHEN meter_type IN (1, 2, 3) THEN 'meter'
-                   WHEN meter_type = 0 THEN 'iv'
-                   WHEN meter_type = {facility_id} THEN 'temp'
-                   ELSE 'other'
-               END AS meter_category
-        FROM (
-            SELECT start_date, meter_id, independent_variable_id, is_independent_variable, 
-                   meter_name, reading, meter_type, facility_id
-            FROM epp.meter_hourly_entries          
-              WHERE is_active = true 
-              AND start_date BETWEEN '{start_date}' AND '{end_date}'
-              AND facility_id = {facility_id} {where_condition}
-
-
-
-
-
-            UNION ALL
-
-            SELECT date_time AS start_date, station_id AS meter_id, 
-                   NULL AS independent_variable_id, false AS is_independent_variable,
-                   station_name AS meter_name, temp AS reading, 104 AS meter_type, 
-                   NULL::integer AS facility_id
-            FROM epp.weather_data_records 
-            WHERE station_id IN ({temp1}, {temp2}, {temp3})
-              AND date_time BETWEEN '{start_date}' AND '{end_date}'
-        ) combined_data
-    )
-    SELECT start_date,
-           meter_id,
-           meter_name,
-           meter_type,
-           CASE 
-               WHEN meter_category = 'meter' THEN SUM(reading)
-               ELSE AVG(reading)
-           END AS reading
-    FROM base_data
-    GROUP BY start_date, meter_id, meter_name, meter_type, meter_category
-    ORDER BY start_date, meter_name;
-    """
-
-# def get_data_cleaning_query(temp1, temp2, temp3, start_date, end_date, facility_id, meter_type, independent_variables=()):
-#     where_condition = f"AND meter_type = {meter_type}"
-#     if independent_variables:
-#         where_condition = f"AND (meter_type = {meter_type} or independent_variable in {independent_variables})"
+# def get_data_cleaning_query(temp1, temp2, temp3, start_date, end_date, facility_id, meter_type):
+#     where_condition = f"AND (meter_type = {meter_type} OR is_independent_variable = true)"
 #     return f"""
 #     WITH base_data AS (
 #         SELECT DATE_TRUNC('hour', start_date::timestamp) AS start_date,
@@ -173,3 +104,134 @@ def get_data_cleaning_query(temp1, temp2, temp3, start_date, end_date, facility_
 #     GROUP BY start_date, meter_id, meter_name, meter_type, meter_category
 #     ORDER BY start_date, meter_name;
 #     """
+
+
+def get_data_cleaning_query(temp1, temp2, temp3, start_date, end_date, facility_id, meter_type):
+    where_condition = f"AND (meter_type = {meter_type} OR is_independent_variable = true)"
+    query = f"""
+    WITH raw_data AS (
+    SELECT start_date, 
+           meter_id, 
+           independent_variable_id, 
+           is_independent_variable,
+           meter_name, 
+           reading, 
+           meter_type, 
+           facility_id
+    FROM epp.meter_hourly_entries          
+    WHERE is_active = true 
+      AND start_date BETWEEN '{start_date}' AND '{end_date}'
+      AND facility_id = {facility_id} 
+      {where_condition}
+    
+    UNION ALL
+
+    SELECT date_time AS start_date, 
+           station_id AS meter_id, 
+           NULL AS independent_variable_id, 
+           false AS is_independent_variable,
+           station_name AS meter_name, 
+           temp AS reading, 
+           104 AS meter_type, 
+           NULL::integer AS facility_id
+    FROM epp.weather_data_records 
+    WHERE station_id IN ({temp1}, {temp2}, {temp3})
+      AND date_time BETWEEN '{start_date}' AND '{end_date}'
+),
+averaged_data AS (
+    SELECT start_date, 
+           meter_id, 
+           meter_name, 
+           meter_type, 
+           AVG(reading) AS average_reading
+    FROM raw_data
+    GROUP BY start_date, meter_id, meter_name, meter_type
+),
+base_data AS (
+    SELECT DATE_TRUNC('hour', start_date::timestamp) AS start_date,
+           meter_id,
+           average_reading AS reading,
+           meter_type,
+           CASE 
+               WHEN meter_type IN (1, 2, 3) THEN 'meter'
+               WHEN meter_type = 0 THEN 'iv'
+               WHEN meter_type = 104 THEN 'temp'
+               ELSE 'other'
+           END AS meter_category,
+           
+           CASE 
+            WHEN meter_id = {temp1} THEN 'Temp1'
+            WHEN meter_id = {temp2} THEN 'Temp2'
+            WHEN meter_id = {temp3} THEN 'Temp3'
+            ELSE meter_name
+        END AS meter_name
+    FROM averaged_data
+)
+SELECT start_date,
+       meter_id,
+       meter_name,
+       meter_type,
+       CASE 
+           WHEN meter_category = 'meter' THEN SUM(reading)
+           ELSE AVG(reading)
+       END AS reading
+       
+       
+FROM base_data
+GROUP BY start_date, meter_id, meter_name, meter_type, meter_category
+ORDER BY start_date, meter_name;
+    """
+
+    # query = f"""
+    # WITH base_data AS (
+    #     SELECT DATE_TRUNC('hour', start_date::timestamp) AS start_date,
+    #            CASE
+    #                WHEN is_independent_variable THEN independent_variable_id
+    #                ELSE meter_id
+    #            END AS meter_id,
+    #            CASE
+    #                WHEN meter_id = {temp1} THEN 'Temp1'
+    #                WHEN meter_id = {temp2} THEN 'Temp2'
+    #                WHEN meter_id = {temp3} THEN 'Temp3'
+    #                ELSE meter_name
+    #            END AS meter_name,
+    #            reading,
+    #            meter_type,
+    #            CASE
+    #                WHEN meter_type IN (1, 2, 3) THEN 'meter'
+    #                WHEN meter_type = 0 THEN 'iv'
+    #                WHEN meter_type = 505 THEN 'temp'
+    #                ELSE 'other'
+    #            END AS meter_category
+    #     FROM (
+    #         SELECT start_date, meter_id, independent_variable_id, is_independent_variable,
+    #                meter_name, reading, meter_type, facility_id
+    #         FROM epp.meter_hourly_entries
+    #           WHERE is_active = true
+    #           AND start_date BETWEEN '{start_date}' AND '{end_date}'
+    #           AND facility_id = {facility_id} {where_condition}
+    #
+    #         UNION ALL
+    #
+    #         SELECT date_time AS start_date, station_id AS meter_id,
+    #                NULL AS independent_variable_id, false AS is_independent_variable,
+    #                station_name AS meter_name, temp AS reading, 104 AS meter_type,
+    #                NULL::integer AS facility_id
+    #         FROM epp.weather_data_records
+    #         WHERE station_id IN ({temp1}, {temp2}, {temp3})
+    #           AND date_time BETWEEN '{start_date}' AND '{end_date}'
+    #     ) combined_data
+    # )
+    # SELECT start_date,
+    #        meter_id,
+    #        meter_name,
+    #        meter_type,
+    #        CASE
+    #            WHEN meter_category = 'meter' THEN SUM(reading)
+    #            ELSE AVG(reading)
+    #        END AS reading
+    # FROM base_data
+    # GROUP BY start_date, meter_id, meter_name, meter_type, meter_category
+    # ORDER BY start_date, meter_name;
+    # """
+    return query
