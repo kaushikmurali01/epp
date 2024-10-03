bbdd5dc64de0f126c949a7f194ff872fb410cf23
Varun Pratap Singh <varun.singh@algoworks.com>
2024-10-03 15:14:25 +0530

    Minor

--- meter_uploader.py
+++ meter_uploader.py
@@ -234,21 +234,26 @@ class DataUploader(BaseDataUploader):
                 concurrent.futures.wait(futures)
                 for future in futures:
                     future.result()  # This will raise any exceptions that occurred during processing
-            excel_buffer = io.BytesIO()
-            self.validator.clean_df.to_csv(excel_buffer, index=False)
-            excel_buffer.seek(0)
-            blob_name = generate_blob_name(extension='csv')
-            file_path = save_file_to_blob(blob_name, excel_buffer)
-            record_id = self.create_file_record_in_table(file_path)
             response_message = "File Uploaded Successfully"
             status_code = 200
             if self.data_len != len(self.validator.clean_df[self.validator.clean_df['is_active'] == True]):
-                removed_records = self.data_len - len(self.validator.clean_df[self.validator.clean_df['is_active'] == True])
-                status_code = 202
+                removed_records = self.data_len - len(
+                    self.validator.clean_df[self.validator.clean_df['is_active'] == True])
                 if len(self.validator.clean_df[self.validator.clean_df['is_active'] == True]) == 0:
+                    status_code = 400
                     response_message = f"All the rows had invalid data. So nothing to process."
                 else:
+                    status_code = 202
                     response_message = f"A few Records were removed while Uploading the data as they had invalid records. {removed_records} Row(s) were removed"
+            file_path, record_id = None, None
+            if status_code in [200, 202]:
+                excel_buffer = io.BytesIO()
+                self.validator.clean_df.to_csv(excel_buffer, index=False)
+                excel_buffer.seek(0)
+                blob_name = generate_blob_name(extension='csv')
+                file_path = save_file_to_blob(blob_name, excel_buffer)
+                record_id = self.create_file_record_in_table(file_path)
+
             return {
                 "status_code": status_code,
                 "success": True,
