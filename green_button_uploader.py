b84f819ff5948575db6980839527d475d10df551
Varun Pratap Singh <varun.singh@algoworks.com>
2024-10-14 15:42:42 +0530

    Minor Changes

--- green_button_uploader.py
+++ green_button_uploader.py
@@ -16,41 +16,39 @@ class GreenDataUploader(DataUploader):
     def process(self):
         try:
             df = Convert(self.xml_file)
-            initial_data_length = len(df)  # Store the initial length of data for comparison later
-            chunk_size = 10000  # Consistent with process_excel
-            self.data_len = initial_data_length  # Record initial data length
-
-            with ThreadPoolExecutor() as executor:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
                 futures = []
-                for i in range(0, len(df), chunk_size):
-                    chunk = df[i:i + chunk_size]
-                    futures.append(executor.submit(self.validator.validate_chunk,
-                                                   chunk))  # Assuming validate_chunk will return cleaned data
-
+                chunk_set = 10000
+                self.data_len += len(df)
+                for i in range(0, len(df), chunk_set):
+                    chunk = df[i:i + chunk_set]
+                    futures.append(
+                        executor.submit(self.process_chunk, chunk, df.columns, self.validator, self.iv, self.meter_id))
                 concurrent.futures.wait(futures)
-                validated_data = pd.concat([future.result() for future in futures])  # Concatenate results
+                for future in futures:
+                    future.result()  # This will raise any exceptions that occurred during processing
 
-            if validated_data.empty:
-                raise ValueError("No valid data to process after validation.")
+            response_message = "File Uploaded Successfully"
+            status_code = 200
 
-            csv_buffer = io.BytesIO()
-            validated_data.to_csv(csv_buffer, index=False)
-            csv_buffer.seek(0)
+            if self.data_len != len(self.validator.clean_df[self.validator.clean_df['is_active'] == True]):
+                removed_records = self.data_len - len(
+                    self.validator.clean_df[self.validator.clean_df['is_active'] == True])
+                if len(self.validator.clean_df[self.validator.clean_df['is_active'] == True]) == 0:
+                    status_code = 400
+                    response_message = f"All the rows had invalid data. So nothing to process."
+                else:
+                    status_code = 202
+                    response_message = f"A few Records were removed while Uploading the data as they had invalid records. {removed_records} Row(s) were removed"
 
-            blob_name = generate_blob_name(extension='csv')
-            file_path = save_file_to_blob(blob_name, csv_buffer)
-            record_id = self.create_file_record_in_table(file_path)
-
-            removed_records = initial_data_length - len(validated_data)
-            if len(validated_data) == 0:
-                status_code = 400
-                response_message = "All the rows had invalid data. So nothing to process."
-            elif removed_records > 0:
-                status_code = 202
-                response_message = f"A few Records were removed while uploading the data as they had invalid records. {removed_records} Row(s) were removed"
-            else:
-                status_code = 200
-                response_message = "File Uploaded Successfully"
+            file_path, record_id = None, None
+            if status_code in [200, 202]:
+                excel_buffer = io.BytesIO()
+                self.validator.clean_df.to_csv(excel_buffer, index=False)
+                excel_buffer.seek(0)
+                blob_name = generate_blob_name(extension='csv')
+                file_path = save_file_to_blob(blob_name, excel_buffer)
+                record_id = self.create_file_record_in_table(file_path)
 
             return {
                 "status_code": status_code,
