f73a747810348cfac8ed963cba035d7de0ad3b6d
Varun Pratap Singh <varun.singh@algoworks.com>
2024-10-21 20:20:52 +0530

    Changes for CRON to Update Missing Temprature

--- cron_update_missing_temprature_data.py
+++ cron_update_missing_temprature_data.py
@@ -12,44 +12,13 @@ from contextlib import contextmanager
 
 from dbconnection import get_db_connection, dbtest, execute_query
 from sql_queries.weather_station_queries import UPDATE_IN_USE, MISSING_TEMPERATURE_QUERY
+from update_weather_stations import COLUMN_MAPPING
 
 # Adjust these parameters based on your system capabilities
 MAX_WORKERS = 100  # Reduced from 10 to 5
 CHUNK_SIZE = 100000  # Number of rows to process at a time
 
 # Column mapping: CSV column name to database column name
-COLUMN_MAPPING = {
-    'Longitude (x)': 'longitude',
-    'Latitude (y)': 'latitude',
-    'Station Name': 'station_name',
-    'Climate ID': 'climate_id',
-    'Date/Time (LST)': 'date_time',
-    'Year': 'year',
-    'Month': 'month',
-    'Day': 'day',
-    'Time (LST)': 'time',
-    'Temp (°C)': 'temp',
-    'Temp Flag': 'temp_flag',
-    'Dew Point Temp (°C)': 'dew_point_temp',
-    'Dew Point Temp Flag': 'dew_point_temp_flag',
-    'Rel Hum (%)': 'rel_hum',
-    'Rel Hum Flag': 'rel_hum_flag',
-    'Precip. Amount (mm)': 'precip_amount',
-    'Precip. Amount Flag': 'precip_amount_flag',
-    'Wind Dir (10s deg)': 'wind_dir',
-    'Wind Dir Flag': 'wind_dir_flag',
-    'Wind Spd (km/h)': 'wind_spd',
-    'Wind Spd Flag': 'wind_spd_flag',
-    'Visibility (km)': 'visibility_km',
-    'Visibility Flag': 'visibility_flag',
-    'Stn Press (kPa)': 'station_press',
-    'Stn Press Flag': 'station_press_flag',
-    'Hmdx': 'hmdx',
-    'Hmdx Flag': 'hmdx_flag',
-    'Wind Chill': 'wind_chill',
-    'Wind Chill Flag': 'wind_chill_flag',
-    'Weather': 'weather'
-}
 
 
 @contextmanager
@@ -63,6 +32,7 @@ def get_db_connection_from_pool():
 
 def rename_columns(df):
     return df.rename(columns=COLUMN_MAPPING)
+
 
 def upload_chunk(conn, chunk, station_id):
     cur = conn.cursor()
@@ -92,7 +62,7 @@ def upload_chunk(conn, chunk, station_id):
                                            temp, temp_flag, dew_point_temp, dew_point_temp_flag, rel_hum, rel_hum_flag, 
                                            precip_amount, precip_amount_flag, wind_dir, wind_dir_flag, wind_spd, wind_spd_flag, 
                                            visibility_km, visibility_flag, station_press, station_press_flag, hmdx, hmdx_flag, 
-                                           wind_chill, wind_chill_flag, weather, station_id)
+                                           wind_chill, wind_chill_flag, weather, utc_start_date, station_id)
             FROM STDIN WITH CSV
         """), output)
 
@@ -125,90 +95,6 @@ def upload_chunk(conn, chunk, station_id):
     finally:
         cur.close()
 
-# def upload_chunk(conn, chunk, station_id):
-#     cur = conn.cursor()
-#
-#     # Rename columns and add station_id
-#     chunk = rename_columns(chunk)
-#     chunk['station_id'] = station_id
-#
-#     # Create a temporary staging table
-#     cur.execute("""
-#         CREATE TEMP TABLE IF NOT EXISTS temp_weather_data_records ON COMMIT DROP AS
-#         SELECT * FROM epp.weather_data_records WITH NO DATA;
-#     """)
-#
-#     # Prepare the data for COPY
-#     output = StringIO()
-#     chunk.to_csv(output, index=False, header=False, quoting=csv.QUOTE_MINIMAL)
-#     output.seek(0)
-#
-#     try:
-#         # Bulk insert into the temporary table
-#         cur.copy_expert(sql.SQL("""
-#             COPY temp_weather_data_records (longitude, latitude, station_name, climate_id, date_time, year, month, day, time,
-#                                            temp, temp_flag, dew_point_temp, dew_point_temp_flag, rel_hum, rel_hum_flag,
-#                                            precip_amount, precip_amount_flag, wind_dir, wind_dir_flag, wind_spd, wind_spd_flag,
-#                                            visibility_km, visibility_flag, station_press, station_press_flag, hmdx, hmdx_flag,
-#                                            wind_chill, wind_chill_flag, weather, station_id)
-#             FROM STDIN WITH CSV
-#         """), output)
-#
-#         # Perform a bulk update from the temporary table
-#         cur.execute("""
-#             UPDATE epp.weather_data_records AS main
-#             SET longitude = temp.longitude, latitude = temp.latitude, station_name = temp.station_name,
-#                 climate_id = temp.climate_id, year = temp.year, month = temp.month, day = temp.day, time = temp.time,
-#                 temp = temp.temp, temp_flag = temp.temp_flag, dew_point_temp = temp.dew_point_temp,
-#                 dew_point_temp_flag = temp.dew_point_temp_flag, rel_hum = temp.rel_hum, rel_hum_flag = temp.rel_hum_flag,
-#                 precip_amount = temp.precip_amount, precip_amount_flag = temp.precip_amount_flag, wind_dir = temp.wind_dir,
-#                 wind_dir_flag = temp.wind_dir_flag, wind_spd = temp.wind_spd, wind_spd_flag = temp.wind_spd_flag,
-#                 visibility_km = temp.visibility_km, visibility_flag = temp.visibility_flag, station_press = temp.station_press,
-#                 station_press_flag = temp.station_press_flag, hmdx = temp.hmdx, hmdx_flag = temp.hmdx_flag,
-#                 wind_chill = temp.wind_chill, wind_chill_flag = temp.wind_chill_flag, weather = temp.weather
-#             FROM temp_weather_data_records temp
-#             WHERE main.date_time = temp.date_time AND main.station_id = temp.station_id;
-#         """)
-#         conn.commit()
-#         return f"Successfully updated data for station {station_id}"
-#     except Exception as e:
-#         conn.rollback()
-#         print(f"Error while updating data for station {station_id}: {str(e)}")
-#         return f"Error while updating data for station {station_id}: {str(e)}"
-#     finally:
-#         cur.close()
-
-
-# def upload_chunk(conn, chunk, station_id):
-#     cur = conn.cursor()
-#
-#     # Rename columns and add station_id
-#     chunk = rename_columns(chunk)
-#     chunk['station_id'] = station_id
-#
-#     # Prepare the data for COPY
-#     output = StringIO()
-#     chunk.to_csv(output, index=False, header=False, quoting=csv.QUOTE_MINIMAL)
-#     output.seek(0)
-#
-#     try:
-#         cur.copy_expert(sql.SQL("""
-#             COPY epp.weather_data_records (longitude, latitude, station_name, climate_id, date_time, year, month, day, time,
-#                                temp, temp_flag, dew_point_temp, dew_point_temp_flag, rel_hum, rel_hum_flag,
-#                                precip_amount, precip_amount_flag, wind_dir, wind_dir_flag, wind_spd, wind_spd_flag,
-#                                visibility_km, visibility_flag, station_press, station_press_flag, hmdx, hmdx_flag,
-#                                wind_chill, wind_chill_flag, weather, station_id)
-#             FROM STDIN WITH CSV
-#         """), output)
-#
-#         conn.commit()
-#         return f"Uploaded chunk for station {station_id}"
-#     except Exception as e:
-#         conn.rollback()
-#         return f"Error uploading chunk for station {station_id}: {str(e)}"
-#     finally:
-#         cur.close()
-
 
 def upload_chunk_with_retry(chunk, station_id, max_retries=3, initial_delay=1):
     retries = 0
@@ -223,16 +109,18 @@ def upload_chunk_with_retry(chunk, station_id, max_retries=3, initial_delay=1):
             time.sleep(initial_delay * (2 ** (retries - 1)))  # Exponential backoff
 
 
-def process_station(station_id, year, month, day, time_frame):
+def process_station(station_id, year, month, day, time_frame, time_zone):
     try:
         url = f"http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID={station_id}&Year={year}&Month={month}&Day={day}&timeframe={time_frame}&submit=Download+Data"
         response = requests.get(url, timeout=30)
         response.raise_for_status()
         csv_data = StringIO(response.content.decode('utf-8'))
         chunk_reader = pd.read_csv(csv_data)
-        lookup_date = datetime.datetime.now() - datetime.timedelta(days=15)
-        chunk_reader['Date/Time (LST)'] = pd.to_datetime(chunk_reader['Date/Time (LST)'])
+        lookup_date = datetime.datetime.now() - datetime.timedelta(days=1)
+        chunk_reader['Date/Time (LST)'] = pd.to_datetime(chunk_reader['Date/Time (LST)'], errors='coerce')
         chunk_reader = chunk_reader[chunk_reader['Date/Time (LST)'] <= lookup_date]
+        chunk_reader['utc_start_date'] = chunk_reader['Date/Time (LST)'].dt.tz_localize(
+            time_zone, ambiguous='NaT', nonexistent='shift_forward').dt.tz_convert('UTC')
         upload_chunk_with_retry(chunk_reader, station_id)
         time.sleep(1)  # Add a small delay between chunk uploads
         # for chunk in chunk_reader:
@@ -256,12 +144,13 @@ def main(stations=None, in_use=False):
             year = record['year']
             month = record['month']
             station_id = record['station_id']
+            time_zone = record['timezone']
             day = 14
             time_frame = 1
 
             print(f"Submitting task for station {station_id}, year {year}, month {month}")
             futures.append(
-                executor.submit(process_station, station_id, year, month, day, time_frame))
+                executor.submit(process_station, station_id, year, month, day, time_frame, time_zone))
             time.sleep(0.1)
 
         for future in concurrent.futures.as_completed(futures):
