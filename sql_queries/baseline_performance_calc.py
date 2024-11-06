2a223e37f64c8c876b641e329ad4b1c75d4677a6
Varun Pratap Singh <varun.singh@algoworks.com>
2024-11-06 14:51:00 +0530

    Minor

--- sql_queries/baseline_performance_calc.py
+++ sql_queries/baseline_performance_calc.py
@@ -10,14 +10,70 @@ FROM epp.baseline_model_output_data,
      jsonb_array_elements(output_data::jsonb) as value
 WHERE facility_id = {} and meter_type={} ORDER BY start_date;
 """
+# PERFORMANCE_OBSERVED_PREDICTED = """
+# SELECT
+#     facility_id,
+#     (value->>'observed')::float as observed,
+#     to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' AS start_date,
+#     (to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' + interval '1 hour') as end_date,
+#     (value->>'predicted')::float as predicted,
+#     (value->>'temperature')::float as temperature
+# FROM epp.scoring_data_output,
+#      jsonb_array_elements(scoring_data::jsonb) as value
+# WHERE facility_id = {} and meter_type={} ORDER BY start_date;
+# """
+#
+# PERFORMANCE_OBSERVED_PREDICTED_P4P = """
+# SELECT
+#     facility_id,
+#     (value->>'observed')::float as observed,
+#     to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' AS start_date,
+#     (to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' + interval '1 hour') as end_date,
+#     (value->>'predicted')::float as predicted,
+#     (value->>'temperature')::float as temperature
+# FROM epp.scoring_data_output,
+#      jsonb_array_elements(scoring_data::jsonb) as value
+# WHERE facility_id = {} and meter_type={} and to_timestamp((value->>'Timestamp')::bigint / 1000) AT TIME ZONE '{}' BETWEEN '{}' AND '{}';
+# """
+#
+# COMBINED = """
+# SELECT
+#     facility_id,
+#     'baseline_model_output_data' as source,
+#     (value->>'observed')::float as observed,
+#     to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' AS start_date,
+#     (to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' + interval '1 hour') as end_date,
+#     (value->>'predicted')::float as predicted,
+#     (value->>'temperature')::float as temperature
+# FROM epp.baseline_model_output_data,
+#      jsonb_array_elements(output_data::jsonb) as value
+# WHERE facility_id = {} and meter_type={} ORDER BY start_date
+#
+# UNION ALL
+#
+# SELECT
+#     facility_id,
+#     'scoring_data_output' as source,
+#     (value->>'observed')::float as observed,
+#     to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' AS start_date,
+#     (to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' + interval '1 hour') as end_date,
+#     (value->>'predicted')::float as predicted,
+#     (value->>'temperature')::float as temperature
+# FROM epp.scoring_data_output,
+#      jsonb_array_elements(scoring_data::jsonb) as value
+# WHERE facility_id = {}
+# ORDER BY start_date;
+# """
+
+
 PERFORMANCE_OBSERVED_PREDICTED = """
 SELECT 
     facility_id,
-    (value->>'observed')::float as observed,
-    to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' AS start_date,
-    (to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' + interval '1 hour') as end_date,
-    (value->>'predicted')::float as predicted,
-    (value->>'temperature')::float as temperature
+    (value->>'observed')::float AS observed,
+    to_timestamp((value->>'Timestamp')::bigint / 1000) AS start_date,
+    to_timestamp((value->>'Timestamp')::bigint / 1000) + interval '1 hour' AS end_date,
+    (value->>'predicted')::float AS predicted,
+    (value->>'temperature')::float AS temperature
 FROM epp.scoring_data_output,
      jsonb_array_elements(scoring_data::jsonb) as value
 WHERE facility_id = {} and meter_type={} ORDER BY start_date;
@@ -26,11 +82,11 @@ WHERE facility_id = {} and meter_type={} ORDER BY start_date;
 PERFORMANCE_OBSERVED_PREDICTED_P4P = """
 SELECT 
     facility_id,
-    (value->>'observed')::float as observed,
-    to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' AS start_date,
-    (to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' + interval '1 hour') as end_date,
-    (value->>'predicted')::float as predicted,
-    (value->>'temperature')::float as temperature
+    (value->>'observed')::float AS observed,
+    to_timestamp((value->>'Timestamp')::bigint / 1000) AS start_date,
+    to_timestamp((value->>'Timestamp')::bigint / 1000) + interval '1 hour' AS end_date,
+    (value->>'predicted')::float AS predicted,
+    (value->>'temperature')::float AS temperature
 FROM epp.scoring_data_output,
      jsonb_array_elements(scoring_data::jsonb) as value
 WHERE facility_id = {} and meter_type={} and to_timestamp((value->>'Timestamp')::bigint / 1000) AT TIME ZONE '{}' BETWEEN '{}' AND '{}';
@@ -39,12 +95,11 @@ WHERE facility_id = {} and meter_type={} and to_timestamp((value->>'Timestamp'):
 COMBINED = """
 SELECT 
     facility_id,
-    'baseline_model_output_data' as source,
-    (value->>'observed')::float as observed,
-    to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' AS start_date,
-    (to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' + interval '1 hour') as end_date,
-    (value->>'predicted')::float as predicted,
-    (value->>'temperature')::float as temperature
+    (value->>'observed')::float AS observed,
+    to_timestamp((value->>'Timestamp')::bigint / 1000) AS start_date,
+    to_timestamp((value->>'Timestamp')::bigint / 1000) + interval '1 hour' AS end_date,
+    (value->>'predicted')::float AS predicted,
+    (value->>'temperature')::float AS temperature
 FROM epp.baseline_model_output_data,
      jsonb_array_elements(output_data::jsonb) as value
 WHERE facility_id = {} and meter_type={} ORDER BY start_date
@@ -53,12 +108,11 @@ UNION ALL
 
 SELECT 
     facility_id,
-    'scoring_data_output' as source,
-    (value->>'observed')::float as observed,
-    to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' AS start_date,
-    (to_timestamp((value->>'Timestamp')::bigint/1000) AT TIME ZONE '{}' + interval '1 hour') as end_date,
-    (value->>'predicted')::float as predicted,
-    (value->>'temperature')::float as temperature
+    (value->>'observed')::float AS observed,
+    to_timestamp((value->>'Timestamp')::bigint / 1000) AS start_date,
+    to_timestamp((value->>'Timestamp')::bigint / 1000) + interval '1 hour' AS end_date,
+    (value->>'predicted')::float AS predicted,
+    (value->>'temperature')::float AS temperature
 FROM epp.scoring_data_output,
      jsonb_array_elements(scoring_data::jsonb) as value
 WHERE facility_id = {}
